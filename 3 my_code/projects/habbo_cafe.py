c12c75b86b7a691ee57ed3fb27625b6c55cb5e14
Matt Johnson <matthew_johnson@loomis.org>
2025-01-23 13:15:53 -0500

    project updates

---
+++ 3 my_code/projects/habbo_cafe.py
@@ -0,0 +1,16 @@
+
+def read_file(filename):
+    #returns a list of data extracted from the parameter file
+    #each element of the returned list is a line from the file.
+    with open(filename) as file:
+        ret = file.read()
+    return ret.split('\n')
+
+#add functions here
+
+
+#Write your main script here\
+def main():
+    menu_list = read_file('menu.txt')
+
+main()
