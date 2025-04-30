6c3055c6d85f2d615f6557a8331467a5016911ad
Matt Johnson <matthew_johnson@loomis.org>
2025-04-30 10:10:46 -0400

    4/30

--- 2 example_code/unit_5/code_along.py
+++ 2 example_code/unit_5/code_along.py
@@ -5,6 +5,20 @@ screen_width = 1280
 screen_height = 960
 
 '''---------------------------------SPRITES/CLASSES---------------------'''
+class Background(pygame.sprite.Sprite):
+    def __init__(self, which):
+        super().__init__()
+        self.image = pygame.image.load('assets/cityclose.png')
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (which*self.rect.width, 0)
+        self.speed = -5
+
+    def update(self):
+        
+        self.rect.x += self.speed
+
+        if self.rect.right < 0:
+            self.rect.x += self.rect.width*2
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, initial_pos, size, color):
@@ -17,34 +31,34 @@ class Player(pygame.sprite.Sprite):
         # Position and Movement
         self.rect = self.image.get_rect(center = initial_pos) # Get the rectangle hitbox, positioned at initial_pos
 
-        # Speed - random initial direction
-        self.speed_x = 5 * random.choice((1, -1)) # Pixels per frame horizontally
-        self.speed_y = 5 * random.choice((1, -1)) # Pixels per frame vertically
+        # Speed
+        self.speed = 5
+
+    def change_look(self):
+        costume_list = ['assets/necklace_green.png', 'assets/necklace_green.png', 'assets/potion_red.png']
+        self.image = pygame.image.load(random.choice(costume_list))
+
 
     def update(self):
-        """Called once per frame. Handles movement and bouncing."""
-        # Move the sprite
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
+        """Called once per frame. Handles player movement based on key presses."""
+        # Get a dictionary of all currently pressed keys
+        keys = pygame.key.get_pressed()
 
-        # Bounce off screen edges
-        # Left edge
-        if self.rect.left <= 0:
-            self.rect.left = 0 # Prevent getting stuck slightly off-screen
-            self.speed_x *= -1 # Reverse horizontal speed
-        # Right edge
-        if self.rect.right >= screen_width:
-            self.rect.right = screen_width # Prevent getting stuck
-            self.speed_x *= -1
-        # Top edge
-        if self.rect.top <= 0:
-            self.rect.top = 0 # Prevent getting stuck
-            self.speed_y *= -1 # Reverse vertical speed
-        # Bottom edge
-        if self.rect.bottom >= screen_height:
-            self.rect.bottom = screen_height # Prevent getting stuck
-            self.speed_y *= -1
-            self.image = pygame.image.load('assets/book_green.png')
+        # Move left/right
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.x += self.speed
+
+        # Move up/down
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.rect.y += self.speed
+
+        # --- Keep Player On Screen ---
+        # Use clamp_ip() to modify the rect *in-place* so it stays within screen bounds
+        self.rect.clamp_ip(screen.get_rect())
 
 '''---------------------------------SETUP-------------------------------'''
 # General setup
@@ -68,6 +82,8 @@ player_sprite = Player(initial_pos=(screen_width // 2, screen_height // 2), size
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player_sprite) # Add our player instance to the group
 
+bg_group = pygame.sprite.Group()
+bg_group.add(Background(0), Background(1))
 
 '''----------------------------------LOOP-------------------------------'''
 while True:
@@ -77,17 +93,24 @@ while True:
             pygame.quit()
             sys.exit()
         # Add other event handling here later (like key presses)
+        if event.type == pygame.KEYDOWN:
+             if event.key == pygame.K_SPACE:
+                 # Change image on spacebar press
+                 for sprite in all_sprites:
+                     sprite.change_look()
 
     # --- Game Logic Updates ---
     # Call the update() method on ALL sprites in the group
+    bg_group.update()
     all_sprites.update() # This automatically calls player_sprite.update()
 
     # --- Drawing ---
-    screen.fill('grey20') # Fill screen with a dark grey color
+    screen.fill('orange') # Fill screen with a dark grey color
 
     # Draw ALL sprites in the group onto the screen surface
+    bg_group.draw(screen)
     all_sprites.draw(screen)
-
+    
     # --- Updating the window ---
     pygame.display.flip() # Show the newly drawn frame
     clock.tick(60)       # Limit frame rate to 60 FPS
